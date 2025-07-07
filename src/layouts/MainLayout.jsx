67571d17a9aedc85ec16ea888e907961d2c104f2
Hunter111 <sukhomudrenkov17@gmail.com>
2025-07-07 12:15:19 +0300

    Fixed footer always showing at the bottom of the page

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -5,7 +5,7 @@ import Footer from './components/Footer'
 
 const MainLayout = () => {
 	return (
-		<div className="flex flex-col min-h-[100%]">
+		<div className="flex flex-col min-h-screen">
 			<Header />
 			<main className="flex-grow flex-shrink-0">
 				<Outlet />
